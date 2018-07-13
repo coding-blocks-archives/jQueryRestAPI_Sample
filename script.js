18c681aa3dffb6facd92964dff3d12563c159f99
Arnav Gupta <arnav@codingblocks.com>
2018-07-13 21:39:54 +0530

    filter todos by user
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
+let todos = []
+let users = []
+
 
 function getUsers(done) {
+  if (localStorage['todos']) {
+    todos = JSON.parse(localStorage['todos'])
+  }
+  if (users && users.length > 0) {
+    return done(users)
+  }
   $.getJSON('http://jsonplaceholder.typicode.com/users', (data) => {
+    localStorage['users'] = JSON.stringify(data)
     done(data)
   })
 }
+
 function getTodos(done) {
+  if (localStorage['users']) {
+    users = JSON.parse(localStorage['users'])
+  }
+  if (todos && todos.length > 0) {
+    return done(todos)
+  }
   $.getJSON('http://jsonplaceholder.typicode.com/todos', (data) => {
+    localStorage['todos'] = JSON.stringify(data)
     done(data)
   })
 }
@@ -24,6 +42,7 @@ function refreshUsers(users) {
                   ${user.address.city}<br>
                   ${user.address.zipcode}
                 </p>
+                <a onclick="showTodosOfUser(${user.id})" href="#" class="card-link">Todos</a>
               </div>
             </div>
           </div>
@@ -31,8 +50,24 @@ function refreshUsers(users) {
     )
   })
 }
-
-function refreshTodos(todos) {
+function showTodosOfUser(userId) {
+  toggleActive('todos')
+  refreshTodos(todos, userId)
+}
+function refreshTodos(todos, filterUserId) {
+  if (filterUserId) {
+    todos = todos.filter((todo) => (todo.userId === filterUserId))
+  }
+  todos.forEach((todo) => {
+    $('#row-todos').append(
+      `
+    <div class="row list-group-item">
+      <span class="col-3"><input type="checkbox" ${todo.completed ? 'checked' : ''}></span>
+      <span class="col ${todo.completed ? 'completed-task': ''}">${todo.title}</span>
+    </div>
+    `
+    )
+  })
 
 }
 function toggleActive(newActiveTab) {
@@ -43,6 +78,11 @@ function toggleActive(newActiveTab) {
 }
 
 $(function () { // means window.onload or document.onready in jquery
+
+  // When page loads, by default show users
+  toggleActive('users')
+  getUsers((users) => refreshUsers(users))
+
 
   $('#tab-users').click(() => {
     toggleActive('users')
